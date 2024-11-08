bd13406cc89dba8076ed16501d17fce47dcd8a30
DAniels0100 <se.alchanto@gmail.com>
2024-11-08 04:33:43 -0600

    Test unitario

--- app/src/test/java/com/example/miprimeraplicacion/TestDatosPropiedad.java
+++ app/src/test/java/com/example/miprimeraplicacion/TestDatosPropiedad.java
@@ -16,7 +16,7 @@ public class TestDatosPropiedad {
         propiedad = new Propiedad(
                 "Casa de Playa",
                 "1200",
-                "Costa Rica",
+                "Guanacaste",
                 "Piscina, WiFi, Aire Acondicionado",
                 6,
                 3,
@@ -35,7 +35,7 @@ public class TestDatosPropiedad {
         // Verificar que los datos en el JSON coinciden con los de la propiedad
         assertEquals("Casa de Playa", jsonObject.optString("nombrePropiedad"));
         assertEquals("1200", jsonObject.optString("precio"));
-        assertEquals("Costa Rica", jsonObject.optString("ubicacion"));
+        assertEquals("Guanacaste", jsonObject.optString("ubicacion"));
         assertEquals("Piscina, WiFi, Aire Acondicionado", jsonObject.optString("amenidadesCasa"));
         assertEquals(6, jsonObject.optInt("cantidadPersonas"));
         assertEquals(3, jsonObject.optInt("cantidadHabitaciones"));
